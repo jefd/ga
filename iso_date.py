20d2359fef5793cb3b7e6f761b044eabf550c3c2
jef <jef.dodson@jpl.nasa.gov>
2023-05-05 11:29:37 -0700

    Rename file.

--- iso_date.py
+++ iso_date.py
@@ -7,11 +7,15 @@ for line in f:
 
     date, count = line.split(',')
     month, day, year = date.split('/')
+
     if len(month) == 1:
         month = '0' + month
     if len(day) == 1:
         day = '0' + day
-    year = f'20{year}'
+
+    if len(year) == 2:            
+        year = f'20{year}'
+
     iso_date = f'{year}-{month}-{day}'
     print(f'{iso_date},{count}')
 
